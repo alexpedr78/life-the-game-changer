e1274fc1a496a329ee62c0b44830044e6d82d980
Flow-Fly <florian.aube@gmail.com>
2024-03-12 15:43:25 +0100

    Suggestions

--- src/Player.js
+++ src/Player.js
@@ -1,135 +1,147 @@
 // screen
-const menu = document.getElementById("landing-page");
-const initializeScreen = document.getElementById("initialize-screen");
-const gameScreen = document.getElementById("game-screen");
-const ruleScreen = document.getElementById("rule-screen");
-const endScreen = document.getElementById("end-screen");
+const menu = document.getElementById("landing-page")
+const initializeScreen = document.getElementById("initialize-screen")
+const gameScreen = document.getElementById("game-screen")
+const ruleScreen = document.getElementById("rule-screen")
+const endScreen = document.getElementById("end-screen")
 
 // button
 
-const ruleButton = document.getElementById("rule-btn");
-const startGameButton = document.getElementById("start-init");
-const restartButton = document.getElementById("restart");
-const initializeButton = document.getElementById("send");
-const backTomenuButton = document.getElementById("menu");
+const ruleButton = document.getElementById("rule-btn")
+const startGameButton = document.getElementById("start-init")
+const restartButton = document.getElementById("restart")
+const initializeButton = document.getElementById("send")
+const backTomenuButton = document.getElementById("menu")
 
 // other
-const gameContainer = document.getElementById("game");
-const cellArray = [];
-let modal = document.getElementById("modal");
+const gameContainer = document.getElementById("game")
+const cellArray = []
+let modal = document.getElementById("modal")
 // let flag = true;
 export class Player {
-  constructor(name) {
-    this.name = name;
-    this.x = 0;
-    this.y = 0;
-    this.position = null;
-    this.cellArray = [];
-  }
+	constructor(name) {
+		this.name = name
+		this.x = 0
+		this.y = 0
+		this.position = null
+		this.cellArray = []
+	}
 
-  comparePosition() {
-    for (let elem of this.cellArray) {
-      if (
-        elem.classList.contains("obstacles") &&
-        elem.classList.contains("playerPosition")
-      ) {
-        elem.classList.remove("playerPosition");
-        this.x -= 2;
-      }
-    }
-    this.position = document.querySelector(
-      `.cell[x="${this.x}"][y="${this.y}"]`
-    );
-    this.position.classList.add("playerPosition");
+	comparePosition() {
+		for (let elem of this.cellArray) {
+			if (
+				elem.classList.contains("obstacles") &&
+				elem.classList.contains("playerPosition")
+			) {
+				elem.classList.remove("playerPosition")
+				this.x -= 2
+			}
+		}
+		this.position = document.querySelector(
+			`.cell[x="${this.x}"][y="${this.y}"]`
+		)
+		this.position.classList.add("playerPosition")
 
-    this.cellArray.forEach((cell) => {
-      cell.classList.remove("unclickable");
-      // cell.removeEventListener("click", this.doThing.bind(this));
-      if (Number(cell.getAttribute("x")) <= this.x) {
-        cell.classList.add("unclickable");
-      } else if (Number(cell.getAttribute("x")) > this.x + 1) {
-        cell.classList.add("unclickable");
-      }
-    });
-    setTimeout(() => {
-      this.loopMove();
-    }, 1000);
-    setTimeout(() => {
-      this.reinitializeBlackBoxes();
-    }, 1500);
-  }
+		this.cellArray.forEach((cell) => {
+			cell.classList.remove("unclickable")
+			// cell.removeEventListener("click", this.doThing.bind(this));
+			if (Number(cell.getAttribute("x")) <= this.x) {
+				cell.classList.add("unclickable")
+			} else if (Number(cell.getAttribute("x")) > this.x + 1) {
+				cell.classList.add("unclickable")
+			}
+		})
+		setTimeout(() => {
+			this.loopMove()
+		}, 1000)
+		setTimeout(() => {
+			this.reinitializeBlackBoxes()
+		}, 1500)
+	}
 
-  loopMove() {
-    // flag = true;
-    // const intervalId = setTimeout(() => {
-    if (this.x < 13) {
-      modal.hidden = false;
-      const upperLine = document.querySelectorAll(`.cell[x="${this.x + 1}"]`);
-      upperLine.forEach((cell) => {
-        cell.addEventListener("click", this.doThing.bind(this));
-      });
-      // flag = false;
-    } else {
-      gameScreen.hidden = true;
-      gameContainer.innerHTML = "";
-      endScreen.hidden = false;
-      // clearInterval(intervalId);
-    }
-    // }, 1000);
-  }
+	moveUp() {
+		this.x++
+	}
 
-  doThing(e) {
-    const cell = e.currentTarget;
-    console.log(cell);
-    modal.hidden = true;
-    if (this.position) {
-      this.position.classList.remove("playerPosition");
-    }
-    cell.classList.add("playerPosition");
-    this.position = cell;
-    this.x = Number(cell.getAttribute("x"));
-    this.y = Number(cell.getAttribute("y"));
-    this.setRandomObstacle();
+	hidePlayer() {
+		if (!this.position) return
+		this.position.classList.remove("playerPosition")
+	}
+	displayPlayer() {
+		this.position.classList.add("playerPosition")
+	}
 
-    this.comparePosition();
-  }
+	loopMove() {
+		// flag = true;
+		// const intervalId = setTimeout(() => {
+		if (this.x < 13) {
+			modal.hidden = false
+			const upperLine = document.querySelectorAll(`.cell[x="${this.x + 1}"]`)
+			upperLine.forEach((cell) => {
+				cell.addEventListener("click", this.doThing.bind(this))
+			})
+			// flag = false;
+		} else {
+			gameScreen.hidden = true
+			gameContainer.innerHTML = ""
+			endScreen.hidden = false
+			// clearInterval(intervalId);
+		}
+		// }, 1000);
+	}
 
-  setRandomObstacle() {
-    const line3 = document.querySelectorAll('.cell[x="3"]');
-    const line6 = document.querySelectorAll('.cell[x="6"]');
-    const line9 = document.querySelectorAll('.cell[x="9"]');
-    const line11 = document.querySelectorAll('.cell[x="11"]');
-    const line12 = document.querySelectorAll('.cell[x="12"]');
+	doThing(e) {
+		const cell = e.currentTarget
+		console.log(cell)
+		modal.hidden = true
+		if (this.position) {
+			this.position.classList.remove("playerPosition")
+		}
+		cell.classList.add("playerPosition")
+		this.position = cell
+		this.x = Number(cell.getAttribute("x"))
+		this.y = Number(cell.getAttribute("y"))
+		this.setRandomObstacle()
 
-    this.randomObstaclesPerLine(line3);
-    this.randomObstaclesPerLine(line3);
-    this.randomObstaclesPerLine(line3);
-    this.randomObstaclesPerLine(line3);
-    this.randomObstaclesPerLine(line3);
-    this.randomObstaclesPerLine(line3);
-    this.randomObstaclesPerLine(line3);
+		this.comparePosition()
+	}
 
-    // console.log(line3);
-  }
-  // TEST
-  randomObstaclesPerLine(line) {
-    let randomNumber = this.createRandomNumber();
-    // line[randomNumber].classList.add("obstacles");
-    line.forEach((cell) => {
-      if (Number(cell.getAttribute("y")) === randomNumber) {
-        cell.classList.add("obstacles");
-      }
-    });
-  }
+	setRandomObstacle() {
+		const line3 = document.querySelectorAll('.cell[x="3"]')
+		const line6 = document.querySelectorAll('.cell[x="6"]')
+		const line9 = document.querySelectorAll('.cell[x="9"]')
+		const line11 = document.querySelectorAll('.cell[x="11"]')
+		const line12 = document.querySelectorAll('.cell[x="12"]')
 
-  createRandomNumber() {
-    let i = Math.ceil(Math.random() * 10);
-    return i;
-  }
+		this.randomObstaclesPerLine(line3)
+		this.randomObstaclesPerLine(line3)
+		this.randomObstaclesPerLine(line3)
+		this.randomObstaclesPerLine(line3)
+		this.randomObstaclesPerLine(line3)
+		this.randomObstaclesPerLine(line3)
+		this.randomObstaclesPerLine(line3)
 
-  reinitializeBlackBoxes() {
-    this.cellArray.forEach((cell) => {
-      cell.classList.remove("obstacles");
-    });
-  }
+		// console.log(line3);
+	}
+	// TEST
+	randomObstaclesPerLine(line) {
+		let randomNumber = this.createRandomNumber()
+		// line[randomNumber].classList.add("obstacles");
+		line.forEach((cell) => {
+			if (Number(cell.getAttribute("y")) === randomNumber) {
+				cell.classList.add("obstacles")
+			}
+		})
+	}
+
+	createRandomNumber() {
+		let i = Math.ceil(Math.random() * 10)
+		return i
+	}
+
+	reinitializeBlackBoxes() {
+		this.cellArray.forEach((cell) => {
+			cell.classList.remove("obstacles")
+		})
+	}
 }
--- src/index.js
+++ src/index.js
@@ -1,70 +1,122 @@
-import { Player } from "./Player.js";
+import { Player } from "./Player.js"
 // screen
-const landingScreen = document.getElementById("landing-page");
-const initializeScreen = document.getElementById("initialize-screen");
-const gameScreen = document.getElementById("game-screen");
-const ruleScreen = document.getElementById("rule-screen");
-const endScreen = document.getElementById("end-screen");
+const landingScreen = document.getElementById("landing-page")
+const initializeScreen = document.getElementById("initialize-screen")
+const gameScreen = document.getElementById("game-screen")
+const ruleScreen = document.getElementById("rule-screen")
+const endScreen = document.getElementById("end-screen")
 
 // button
 
-const ruleButton = document.getElementById("rule-btn");
-const startGameButton = document.getElementById("start-init");
-const restartButton = document.getElementById("restart");
-const initializeButton = document.getElementById("send");
-const backTomenuButton = document.getElementById("menu");
+const ruleButton = document.getElementById("rule-btn")
+const startGameButton = document.getElementById("start-init")
+const restartButton = document.getElementById("restart")
+const initializeButton = document.getElementById("send")
+const backTomenuButton = document.getElementById("menu")
 
 // other
-const gameContainer = document.getElementById("game");
-const cellArray = [];
-let modal = document.getElementById("modal");
+const gameContainer = document.getElementById("game")
+const cellArray = []
+let player
+let modal = document.getElementById("modal")
+let timeoutId = null
 
 function createBoard() {
-  const player1 = new Player("alexis");
+	player = new Player("alexis")
 
-  for (let i = 13; i > 0; i--) {
-    let divLine = document.createElement("div");
-    divLine.classList.add("divLine");
-    for (let j = 10; j > 0; j--) {
-      const cell = document.createElement("div");
-      cell.classList.add("cell");
-      //   cell.textContent = i;
-      cell.setAttribute("x", i);
-      cell.setAttribute("y", j);
-      player1.cellArray.push(cell);
-      divLine.append(cell);
-    }
-    gameContainer.append(divLine);
-  }
+	for (let i = 12; i >= 0; i--) {
+		let divLine = document.createElement("div")
+		divLine.classList.add("divLine")
+		for (let j = 0; j < 10; j++) {
+			const cell = document.createElement("div")
+			cell.classList.add("cell")
+			// cell.textContent = `i: ${i}, j ${j}`
+			cell.setAttribute("x", i)
+			cell.setAttribute("y", j)
+			player.cellArray.push(cell)
+			divLine.append(cell)
+		}
+		gameContainer.append(divLine)
+	}
 
-  player1.loopMove();
+	// player.loopMove()
 }
 
 initializeButton.addEventListener("click", () => {
-  initializeScreen.hidden = true;
-  gameScreen.hidden = false;
-  createBoard();
-});
+	initializeScreen.hidden = true
+	gameScreen.hidden = false
+	createBoard()
+})
 
 ruleButton.addEventListener("click", () => {
-  landingScreen.hidden = true;
-  ruleScreen.hidden = false;
-});
+	landingScreen.hidden = true
+	ruleScreen.hidden = false
+})
 
 backTomenuButton.addEventListener("click", () => {
-  gameContainer.innerHTML = "";
-  ruleScreen.hidden = true;
-  landingScreen.hidden = false;
-});
+	gameContainer.innerHTML = ""
+	ruleScreen.hidden = true
+	landingScreen.hidden = false
+})
 
 restartButton.addEventListener("click", () => {
-  gameContainer.innerHTML = "";
-  createBoard();
-});
+	gameContainer.innerHTML = ""
+	createBoard()
+})
 
 startGameButton.addEventListener("click", () => {
-  landingScreen.hidden = true;
-  initializeScreen.hidden = false;
-});
+	landingScreen.hidden = true
+	initializeScreen.hidden = false
+})
 
-gameContainer.addEventListener('click', (event) => )
+gameContainer.addEventListener("click", (event) => {
+	if (timeoutId) return
+	const clickedCell = event.target
+	console.log(`Are we clicking on the next line?`)
+	console.log(player.x)
+	console.log(clickedCell)
+	let xFromHtmlElement = Number(clickedCell.getAttribute("x"))
+	if (player.x + 1 === xFromHtmlElement) {
+		console.log("We clicked on the next line, do something")
+		player.moveUp()
+		player.hidePlayer()
+		player.position = clickedCell
+
+		player.displayPlayer()
+		setRandomObstacle(2)
+		setRandomObstacle(2)
+		setRandomObstacle(2)
+		setRandomObstacle(2)
+		setRandomObstacle(2)
+		setRandomObstacle(2)
+		setRandomObstacle(2)
+		setRandomObstacle(2)
+		setRandomObstacle(3)
+		setRandomObstacle(4)
+
+		console.log(player.position)
+
+		if (player.position.classList.contains("obstacles")) {
+			// move down etc.
+		}
+
+		timeoutId = setTimeout(() => {
+			document.querySelectorAll(".obstacles").forEach((cell) => {
+				cell.classList.remove("obstacles")
+				timeoutId = null
+			})
+		}, 1000)
+	} else {
+		console.log("We clicked on an other line, do nothing.")
+	}
+})
+
+function setRandomObstacle(num) {
+	const line = document.querySelectorAll(`.cell[x="${num}"]`)
+	randomObstaclePerLine(line)
+}
+
+function randomObstaclePerLine(line) {
+	let randomNumber = Math.floor(Math.random() * line.length)
+	line[randomNumber].classList.add("obstacles")
+}
